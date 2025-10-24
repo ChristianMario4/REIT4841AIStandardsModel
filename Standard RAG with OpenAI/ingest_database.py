b31ef54510dd887c1fa0a93e310a45f0d1e5151a
ChristianMario4 <almario900@gmail.com>
2025-10-24 13:16:23 +1000

    Changed chunk_size in ingest_db to 500 for testing

--- Standard RAG with OpenAI/ingest_database.py
+++ Standard RAG with OpenAI/ingest_database.py
@@ -81,7 +81,7 @@ def main():
 
     print("Splitting documents into chunks...")
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=300,
+        chunk_size=500,
         chunk_overlap=100,
         length_function=len,
         is_separator_regex=False,
