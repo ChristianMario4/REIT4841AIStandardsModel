a0ca38b9262e7f4fdbcf19a0f1356cd276ad3fb9
ChristianMario4 <almario900@gmail.com>
2025-10-24 10:04:25 +1000

    Testing impacts of changes on 5 k-sim

--- Standard RAG with OpenAI/chatbot.py
+++ Standard RAG with OpenAI/chatbot.py
@@ -47,7 +47,7 @@ index = pc.Index(index_name)
 
 
 # Configuration for embeddings and retrieval
-num_results = 7
+num_results = 5
 simscore_threshold = 0.5
 
 # Fix to resolve re-current re-initialisation. Cache_resource makes sure this only occurs once.
