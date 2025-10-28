bba933b1f02ee4aa3f1a74ca44792851ccdad0bb
ChristianMario4 <almario900@gmail.com>
2025-10-28 21:22:12 +1000

    Changed num_results to 5 to return to base model

--- Standard RAG with OpenAI/chatbot.py
+++ Standard RAG with OpenAI/chatbot.py
@@ -50,8 +50,8 @@ index = pc.Index(index_name)
 
 
 # Configuration for embeddings and retrieval
-num_results = 7
-simscore_threshold = 0.5
+num_results = 5
+simscore_threshold = 0.3
 
 # Fix to resolve re-current re-initialisation. Cache_resource makes sure this only occurs once.
 @st.cache_resource # Claude recommended bug fix for syncing error thrown by Streamlit
