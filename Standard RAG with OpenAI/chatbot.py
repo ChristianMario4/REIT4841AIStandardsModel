ec85c1fe0124adff573702d83ddd99fa411b6f8f
ChristianMario4 <almario900@gmail.com>
2025-11-04 10:50:12 +1000

    Changed num_results to 7 for re-testing

--- Standard RAG with OpenAI/chatbot.py
+++ Standard RAG with OpenAI/chatbot.py
@@ -49,7 +49,7 @@ index = pc.Index(index_name)
 
 
 # Configuration for embeddings and retrieval
-num_results = 5
+num_results = 7
 simscore_threshold = 0.5    
 
 # Fix to resolve re-current re-initialisation. Cache_resource makes sure this only occurs once.
